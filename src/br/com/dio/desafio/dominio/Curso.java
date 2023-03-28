2419bb6745f0ced7eca06852135cf5d261584e8b
weltonlopes <welton.lps@hotmail.com>
2023-03-28 06:27:40 -0300

    Add classes Dev e Bootcamp

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,24 +1,11 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
+
     private int cargaHoraria;
 
     public Curso(){}
 
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -27,7 +14,12 @@ public class Curso {
     }
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + this.getTitulo() + ", descricao=" + this.getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+    }
+
+    @Override
+    public double calcularXp(){
+        return XP_PADRAO * cargaHoraria;
     }
 
     
